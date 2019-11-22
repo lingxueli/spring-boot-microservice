f4059a9ada2fc6e41ddbb9de688d41466c1a6f2d
lingxueli <ll2871@columbia.edu>
2019-11-21 23:16:55 -0700

    sql used to import data

---
+++ dogMicroservice/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO dog (id, name, breed, origin) VALUES (1, 'Bella','Golden Retriever','UK');
+INSERT INTO dog (id, name, breed, origin) VALUES (2, 'Lucy','Havanese','Cuba');
+INSERT INTO dog (id, name, breed, origin) VALUES (3, 'Daisy','Hokkaido','Japan');
+INSERT INTO dog (id, name, breed, origin) VALUES (4, 'Luna','Japanese Spitz','Japan');
+INSERT INTO dog (id, name, breed, origin) VALUES (5, 'Lola','Bichon Frisé','Spain');
+INSERT INTO dog (id, name, breed, origin) VALUES (6, 'Sadie','Labrador Husky','Canada');
