bbad9db2d5823a2dc2c1e379733a27c9f14864d9
lingxueli <ll2871@columbia.edu>
2019-11-21 23:06:58 -0700

    add entity

---
+++ dogMicroservice/src/main/java/com/example/dogMicroservice/entity/Dog.java
@@ -0,0 +1,48 @@
+package com.example.dogMicroservice.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "dog")
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+    private String breed;
+    private String origin;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setName(String name) {
+        name = name;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+}
